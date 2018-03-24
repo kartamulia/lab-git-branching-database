f5537437a95617a7d2427a719eadf122334ac921
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 23:13:36 +0700

    Update database project from database schema changes.

--- Lab Git Branching Database/Lab.GBD.SqlServerDbProject/dbo/Tables/InventoryItems.sql
+++ Lab Git Branching Database/Lab.GBD.SqlServerDbProject/dbo/Tables/InventoryItems.sql
@@ -1,8 +1,16 @@
 ﻿CREATE TABLE [dbo].[InventoryItems] (
-    [Id]            INT             IDENTITY (1, 1) NOT NULL,
-    [Name]          NVARCHAR (MAX)  NULL,
-    [UnitOfMeasure] INT             NOT NULL,
-    [UnitPrice]     DECIMAL (18, 2) NOT NULL,
-    CONSTRAINT [PK_InventoryItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]                  INT             IDENTITY (1, 1) NOT NULL,
+    [Name]                NVARCHAR (MAX)  NULL,
+    [UnitOfMeasurementId] INT             NOT NULL,
+    [UnitPrice]           DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_InventoryItems] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_InventoryItems_UnitOfMeasurements_UnitOfMeasurementId] FOREIGN KEY ([UnitOfMeasurementId]) REFERENCES [dbo].[UnitOfMeasurements] ([Id]) ON DELETE CASCADE
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_InventoryItems_UnitOfMeasurementId]
+    ON [dbo].[InventoryItems]([UnitOfMeasurementId] ASC);
 
---
+++ Lab Git Branching Database/Lab.GBD.SqlServerDbProject/dbo/Tables/UnitOfMeasurements.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[UnitOfMeasurements] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_UnitOfMeasurements] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
