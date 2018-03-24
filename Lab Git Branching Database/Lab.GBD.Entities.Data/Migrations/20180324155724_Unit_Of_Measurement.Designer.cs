19f305f5f3d4fdc9c3b2f3d37b05be341f189704
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 22:58:11 +0700

    Add migration.

--- Lab Git Branching Database/Lab.GBD.Entities.Data/Migrations/20180324155724_Unit_Of_Measurement.Designer.cs
+++ Lab Git Branching Database/Lab.GBD.Entities.Data/Migrations/20180324155724_Unit_Of_Measurement.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Lab.GBD.Entities.Data.Migrations
 {
     [DbContext(typeof(GBDDbContext))]
-    partial class GBDDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180324155724_Unit_Of_Measurement")]
+    partial class Unit_Of_Measurement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lab Git Branching Database/Lab.GBD.Entities.Data/Migrations/20180324155724_Unit_Of_Measurement.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Lab.GBD.Entities.Data.Migrations
+{
+    public partial class Unit_Of_Measurement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UnitOfMeasure",
+                table: "InventoryItems",
+                newName: "UnitOfMeasurementId");
+
+            migrationBuilder.CreateTable(
+                name: "UnitOfMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnitOfMeasurements", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryItems_UnitOfMeasurementId",
+                table: "InventoryItems",
+                column: "UnitOfMeasurementId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InventoryItems_UnitOfMeasurements_UnitOfMeasurementId",
+                table: "InventoryItems",
+                column: "UnitOfMeasurementId",
+                principalTable: "UnitOfMeasurements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InventoryItems_UnitOfMeasurements_UnitOfMeasurementId",
+                table: "InventoryItems");
+
+            migrationBuilder.DropTable(
+                name: "UnitOfMeasurements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryItems_UnitOfMeasurementId",
+                table: "InventoryItems");
+
+            migrationBuilder.RenameColumn(
+                name: "UnitOfMeasurementId",
+                table: "InventoryItems",
+                newName: "UnitOfMeasure");
+        }
+    }
+}
