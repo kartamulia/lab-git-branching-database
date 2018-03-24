39ffe3dd644c6fb6f7c9605a8da1a1d95c0c3114
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 22:43:51 +0700

    Import database schema to database project.

---
+++ Lab Git Branching Database/Lab.GBD.SqlServerDbProject/dbo/Tables/InventoryItems.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[InventoryItems] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (MAX)  NULL,
+    [UnitOfMeasure] INT             NOT NULL,
+    [UnitPrice]     DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_InventoryItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
