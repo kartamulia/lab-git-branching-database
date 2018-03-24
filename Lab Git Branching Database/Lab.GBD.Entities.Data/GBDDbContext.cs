685e6bb9103ac6f7c6a102830e9a4b5b8382c001
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 22:30:02 +0700

    Add data context.

---
+++ Lab Git Branching Database/Lab.GBD.Entities.Data/GBDDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.GBD.Entities.Data
+{
+    public class GBDDbContext : DbContext
+    {
+        public GBDDbContext()
+        {
+        }
+
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GBDDb;Trusted_Connection=True;");
+        }
+    }
+}
