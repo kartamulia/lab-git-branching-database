ba883a2b798d351b7fe46ba8dc731684768d53ec
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 22:41:33 +0700

    Coding add and read inventory items.

--- Lab Git Branching Database/Lab.GBD.ConsoleApp/Program.cs
+++ Lab Git Branching Database/Lab.GBD.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab.GBD.Entities.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,60 @@ namespace Lab.GBD.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var dbContext = new GBDDbContext();
+
+            try
+            {
+
+                AddInventoryItem(dbContext);
+                ReadInventoryItems(dbContext);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (dbContext != null) dbContext.Dispose();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void AddInventoryItem(GBDDbContext dbContext)
+        {
+            try
+            {
+                var result = dbContext.InventoryItems.Add(new Entities.InventoryItem()
+                {
+                    Name = "Inventory Item 1",
+                    UnitOfMeasure = Entities.UnitOfMeasure.Box,
+                    UnitPrice = 100
+                });
+
+                dbContext.SaveChanges();
+
+                Console.WriteLine($"Inventory Item {result.Entity.Name} with Id: {result.Entity.Id} added.");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void ReadInventoryItems(GBDDbContext dbContext)
+        {
+            try
+            {
+                var inventoryItems = dbContext.InventoryItems.ToList();
+
+                Console.WriteLine();
+                inventoryItems?.ForEach(x => Console.WriteLine($"Id: {x.Id}; Name: {x.Name}; UoM: {x.UnitOfMeasure}; Price: {x.UnitPrice}"));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
