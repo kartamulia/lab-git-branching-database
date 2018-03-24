2f8db130da5aa77b54b65c82dfb08e35ea226a21
Muhammad Yamin <kartamulia@outlook.com>
2018-03-24 22:12:50 +0700

    Add inventory item and unit of measure.

---
+++ Lab Git Branching Database/Lab.GBD.Entities/InventoryItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.GBD.Entities
+{
+    public class InventoryItem
+    {
+        public InventoryItem()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public UnitOfMeasure UnitOfMeasure { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ Lab Git Branching Database/Lab.GBD.Entities/UnitOfMeasure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.GBD.Entities
+{
+    public enum UnitOfMeasure
+    {
+        Unknown,
+        Each,
+        Box,
+        Case,
+        Dozen,
+        Inch,
+        Foot,
+        Yard,
+        Meter,
+        Ounce,
+        Pound,
+        Kilogram,
+        Quart,
+        Gallon,
+        Liter,
+        Acre,
+        SquareMeter,
+        Minute,
+        Hour,
+        Day
+    }
+}
